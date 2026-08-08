226226500ab5347a76e1e6d609b33f0530fd9635
dillacorn <28743863+dillacorn@users.noreply.github.com>
2026-08-08 11:41:25 -0400

    Add per-display launcher size locks

--- config/hypr/scripts/quickshell_application_state.sh
+++ config/hypr/scripts/quickshell_application_state.sh
@@ -55,9 +55,58 @@ validate_int_range() {
     }
 }
 
+lock_size() {
+    local monitor="$1" width="$2" height="$3" tmp
+    [[ -n "$monitor" ]] || { printf 'monitor is required\n' >&2; exit 2; }
+    validate_int_range "$width" "$MIN_WIDTH" "$MAX_WIDTH" 'width'
+    validate_int_range "$height" "$MIN_HEIGHT" "$MAX_HEIGHT" 'height'
+    ensure_state
+    tmp="${STATE_FILE}.tmp.$$"
+    jq \
+        --arg monitor "$monitor" \
+        --argjson width "$width" \
+        --argjson height "$height" '
+        .monitors = (.monitors // {})
+        | .monitors[$monitor] = (.monitors[$monitor] // {})
+        | .monitors[$monitor].application_view = {
+            width:$width,
+            height:$height,
+            locked:true
+        }
+        | del(.application_view)
+    ' "$STATE_FILE" >"$tmp"
+    mv -f "$tmp" "$STATE_FILE"
+}
+
+unlock_size() {
+    local monitor="$1" tmp
+    [[ -n "$monitor" ]] || { printf 'monitor is required\n' >&2; exit 2; }
+    ensure_state
+    tmp="${STATE_FILE}.tmp.$$"
+    jq --arg monitor "$monitor" '
+        .monitors = (.monitors // {})
+        | if .monitors[$monitor] then del(.monitors[$monitor].application_view) else . end
+        | del(.application_view)
+    ' "$STATE_FILE" >"$tmp"
+    mv -f "$tmp" "$STATE_FILE"
+}
+
+reset_locks() {
+    local tmp
+    ensure_state
+    tmp="${STATE_FILE}.tmp.$$"
+    jq '
+        .monitors = (.monitors // {})
+        | .monitors |= with_entries(.value |= del(.application_view))
+        | del(.application_view)
+    ' "$STATE_FILE" >"$tmp"
+    mv -f "$tmp" "$STATE_FILE"
+}
+
+# Legacy commands remain accepted so older local helpers do not fail during the
+# testing branch transition. Launcher dimensions are no longer read globally.
 set_field() {
     local field="$1" value="$2" min max label tmp
-
     case "$field" in
         width) min=$MIN_WIDTH; max=$MAX_WIDTH; label='width' ;;
         height) min=$MIN_HEIGHT; max=$MAX_HEIGHT; label='height' ;;
@@ -65,59 +114,38 @@ set_field() {
         icon_size) min=$MIN_ICON_SIZE; max=$MAX_ICON_SIZE; label='icon size' ;;
         *) printf 'invalid field: %s\n' "$field" >&2; exit 2 ;;
     esac
-
     validate_int_range "$value" "$min" "$max" "$label"
     ensure_state
     tmp="${STATE_FILE}.tmp.$$"
     jq --arg field "$field" --argjson value "$value" '
-        .application_view = ({
-            width:420,
-            height:582,
-            text_size:14,
-            icon_size:18,
-            customized:true
-        } * (.application_view // {}))
+        .application_view = (.application_view // {})
         | .application_view[$field] = $value
-        | .application_view.customized = true
-        | .monitors = (.monitors // {})
-        | .monitors |= with_entries(.value |= del(.application_view))
     ' "$STATE_FILE" >"$tmp"
     mv -f "$tmp" "$STATE_FILE"
 }
 
 set_size() {
-    local width="$1" height="$2" tmp
+    local width="$1" height="$2"
     validate_int_range "$width" "$MIN_WIDTH" "$MAX_WIDTH" 'width'
     validate_int_range "$height" "$MIN_HEIGHT" "$MAX_HEIGHT" 'height'
     ensure_state
-    tmp="${STATE_FILE}.tmp.$$"
+    local tmp="${STATE_FILE}.tmp.$$"
     jq --argjson width "$width" --argjson height "$height" '
-        .application_view = ({
-            width:420,
-            height:582,
-            text_size:14,
-            icon_size:18,
-            customized:true
-        } * (.application_view // {}))
+        .application_view = (.application_view // {})
         | .application_view.width = $width
         | .application_view.height = $height
-        | .application_view.customized = true
-        | .monitors = (.monitors // {})
-        | .monitors |= with_entries(.value |= del(.application_view))
     ' "$STATE_FILE" >"$tmp"
     mv -f "$tmp" "$STATE_FILE"
 }
 
 set_all() {
-    local width="$1" height="$2" text_size="$3" icon_size="$4" tmp
-
+    local width="$1" height="$2" text_size="$3" icon_size="$4"
     validate_int_range "$width" "$MIN_WIDTH" "$MAX_WIDTH" 'width'
     validate_int_range "$height" "$MIN_HEIGHT" "$MAX_HEIGHT" 'height'
     validate_int_range "$text_size" "$MIN_TEXT_SIZE" "$MAX_TEXT_SIZE" 'text size'
     validate_int_range "$icon_size" "$MIN_ICON_SIZE" "$MAX_ICON_SIZE" 'icon size'
-
     ensure_state
-    tmp="${STATE_FILE}.tmp.$$"
+    local tmp="${STATE_FILE}.tmp.$$"
     jq \
         --argjson width "$width" \
         --argjson height "$height" \
@@ -127,35 +155,29 @@ set_all() {
             width:$width,
             height:$height,
             text_size:$text_size,
-            icon_size:$icon_size,
-            customized:true
+            icon_size:$icon_size
         }
-        | .monitors = (.monitors // {})
-        | .monitors |= with_entries(.value |= del(.application_view))
     ' "$STATE_FILE" >"$tmp"
     mv -f "$tmp" "$STATE_FILE"
 }
 
 reset_defaults() {
-    local tmp
-    ensure_state
-    tmp="${STATE_FILE}.tmp.$$"
-    jq '
-        .application_view = {
-            width:420,
-            height:582,
-            text_size:14,
-            icon_size:18,
-            customized:false
-        }
-        | .monitors = (.monitors // {})
-        | .monitors |= with_entries(.value |= del(.application_view))
-    ' "$STATE_FILE" >"$tmp"
-    mv -f "$tmp" "$STATE_FILE"
+    reset_locks
 }
 
 cmd="${1:-}"
 case "$cmd" in
+    lock-size)
+        [[ -n ${2:-} && -n ${3:-} && -n ${4:-} ]] || exit 2
+        lock_size "$2" "$3" "$4"
+        ;;
+    unlock-size)
+        [[ -n ${2:-} ]] || exit 2
+        unlock_size "$2"
+        ;;
+    reset-locks)
+        reset_locks
+        ;;
     set)
         [[ -n ${2:-} && -n ${3:-} ]] || exit 2
         set_field "$2" "$3"
@@ -172,7 +194,7 @@ case "$cmd" in
         reset_defaults
         ;;
     *)
-        printf 'usage: %s {set <width|height|text_size|icon_size> <value>|set-size <width> <height>|set-all <width> <height> <text_size> <icon_size>|reset}\n' "${0##*/}" >&2
+        printf 'usage: %s {lock-size <MON> <width> <height>|unlock-size <MON>|reset-locks|set <field> <value>|set-size <width> <height>|set-all <width> <height> <text_size> <icon_size>|reset}\n' "${0##*/}" >&2
         exit 2
         ;;
 esac
