f2c195fbc7f221b5b64c8a8a36bceca6e897d29f
dillacorn <28743863+dillacorn@users.noreply.github.com>
2026-08-15 16:39:25 -0400

    Fix helper regression ShellCheck

--- tests/test-scxctl-helper-update.sh
+++ tests/test-scxctl-helper-update.sh
@@ -15,13 +15,13 @@ require_literal() {
 
 require_literal 'repair_scxctl_update_helper()'
 require_literal 'local destination="/usr/local/libexec/awtarchy/scxctl-helper"'
-require_literal 'local source="${repo_dir}/local/libexec/awtarchy/scxctl-helper"'
-require_literal 'sudo /usr/bin/install -d -m 0755 -o root -g root "$destination_dir"'
-require_literal 'sudo /usr/bin/install -m 0755 -o root -g root "$source" "$temporary"'
-require_literal 'sudo /usr/bin/bash -n "$temporary"'
-require_literal 'sudo /usr/bin/mv -Tf -- "$temporary" "$destination"'
-require_literal 'repair_scxctl_update_helper "$repo_dir"'
-require_literal 'source_hash="$(/usr/bin/sha256sum "$source" | /usr/bin/awk '\''{print $1}'\'')"'
-require_literal 'installed_hash="$(sudo /usr/bin/sha256sum "$temporary" | /usr/bin/awk '\''{print $1}'\'')"'
+require_literal "local source=\"\${repo_dir}/local/libexec/awtarchy/scxctl-helper\""
+require_literal "sudo /usr/bin/install -d -m 0755 -o root -g root \"\$destination_dir\""
+require_literal "sudo /usr/bin/install -m 0755 -o root -g root \"\$source\" \"\$temporary\""
+require_literal "sudo /usr/bin/bash -n \"\$temporary\""
+require_literal "sudo /usr/bin/mv -Tf -- \"\$temporary\" \"\$destination\""
+require_literal "repair_scxctl_update_helper \"\$repo_dir\""
+require_literal "source_hash=\"\$(/usr/bin/sha256sum \"\$source\" | /usr/bin/awk '{print \$1}')\""
+require_literal "installed_hash=\"\$(sudo /usr/bin/sha256sum \"\$temporary\" | /usr/bin/awk '{print \$1}')\""
 
 printf 'PASS: normal updater provisions the trusted scxctl helper.\n'
